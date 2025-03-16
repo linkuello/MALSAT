8bfec0afd28f71ad9b5b076f4bb658a412b95dca
kenebaev <Kutmankenebaev200@gmail.com>
2025-03-16 22:32:25 +0600

    changed Product model

---
+++ src/main/java/com/example/buysell/models/Image.java
@@ -0,0 +1,24 @@
+package com.example.buysell.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "images")
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String originalFileName;
+    private Long size;
+    private String contentType;
+    private boolean isPreviewImage;
+    @Lob
+    private byte[] bytes;
+
+}
